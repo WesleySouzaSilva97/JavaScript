85e47a5692299680ab768dd615078a0cb5b84bb8
Wesley de Souza da Silva <wesleyondo@gmail.com>
2024-09-13 21:33:04 -0300

    estudo das variáveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,21 @@
+/**
+ *  Estudos de contantes e variáveis
+ *  @author Wesley Souza
+ */
+
+console.clear
+
+let nome 
+let idade
+let vip 
+
+nome = "Wesley Souza"
+console.log(typeof(nome))
+console.log(nome) 
+
+idade = 56
+console.log(typeof(idade))
+console.log(idade)
+
+vip.log(typeof(vip))
+console.log(vip) 
