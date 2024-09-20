788760c9b23858be141336d2e1b4e1a1d7de4179
Wesley de Souza da Silva <wesleyondo@gmail.com>
2024-09-19 22:11:02 -0300

    ficha de academia

---
+++ exercicios/academia.js
@@ -0,0 +1,27 @@
+/**
+ * *Ficha de um aluno da cademia
+ * @author Wesley de Souza da Silva
+ */
+
+//variaveis
+let nome  
+let idade
+let peso
+let altura
+let vip 
+
+console.clear()
+nome = "Wesley Souza"
+idade = 27
+peso = 110
+altura = 1.95
+vip = true
+
+//saida
+console.log("Ficha do aluno")
+console.log("_____________________")
+console.log(`Nome: ${nome}`)
+console.log(`Idade: ${idade}`)
+console.log(`Peso: ${peso}`)
+console.log(`Altura: ${altura}`)
+console.log(`Vip: ${vip}`)
