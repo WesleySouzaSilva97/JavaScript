c066be950d8ca47ee3bb801e5838a7caeea0fd2b
Wesley de Souza da Silva <wesleyondo@gmail.com>
2024-09-24 20:05:35 -0300

    mandamentos da programação

---
+++ fundamentos/operadores.js
@@ -0,0 +1,13 @@
+/**
+ * Operadores aritmétricos
+ * @author Wesley Souza
+ */
+
+console.clear()
+
+console.log("Operador           | simbologia")
+console.log("Soma               |          +")
+console.log("Subtração          |          -")
+console.log("Multiplicação      |          *")
+console.log("Divisão            |          /")
+console.log("Módulo             |          %")
