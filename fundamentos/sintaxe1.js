fde973f65429f5dfffcae0884184b1d35cc67c9f
Wesley de Souza da Silva <wesleyondo@gmail.com>
2024-09-24 22:23:56 -0300

    mandamentos da programação

---
+++ fundamentos/sintaxe1.js
@@ -0,0 +1,19 @@
+/**
+ * Sintaxe básica da linguagem JS
+ * @author Wesley Souza
+ */
+
+// Limpar a tela do console
+console.clear()
+
+// JS - Sentença e ;(ponto e virgula) é opcional
+console.log ("Hello World")
+
+// concatenação (unir textos e variáveis)
+let nome = "Wesley Souza"
+console.log(`Nome: ${nome}`)
+console.log("Nome: " + nome) // não recomendavel
+
+// arredondamento (casas decimais)
+let media = 5.789
+console.log(`média: ${media.toFixed(1)}`)
