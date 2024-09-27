0ae090569a673d5f89d80f8e47c24d76a4b29972
Wesley de Souza da Silva <wesleyondo@gmail.com>
2024-09-27 19:56:12 -0300

    media3

---
+++ exercicios/media3.js
@@ -0,0 +1,25 @@
+/**
+ * Cálculo da média aritimetica de 3 valores
+ * @author Wesley Souza
+ */
+
+// Importar a biblioteca read-linesync
+const input = require('readline-sync')
+
+// variaveis
+let num1, num2, num3, media //criei a variavel
+
+console.clear()
+console.log("cálculo da média aritimética de 3 números")
+
+// Entrada
+// No console entradas (input) númericas precisam ser convertidas com uso dp método Number()
+num1 = Number(input.question("Digite o primeiro numero: "))
+num2 = Number(input.question("Digite o segundo numero: "))
+num3 = Number(input.question("Digite o terceiro numero: "))
+
+// processamento
+media = (num1 + num2 + num3) / 3
+
+// saída
+console.log(`Média: ${media.toFixed(1)}`)
