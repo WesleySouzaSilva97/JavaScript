8e291863010298edc8529701ac3b08e103c25641
Wesley Souza <wesleyondo@gmail.com>
2024-10-01 22:16:36 -0300

    correção dos exercicios

---
+++ exercicios/consumo.js
@@ -0,0 +1,20 @@
+/**
+ * Cálculo do consumo do combustível
+ * @author Wesley Souza 
+ */
+
+const input = require('readline-sync')
+
+// Variáveis - O algoritmo
+let distancia, litros, consumo
+
+console.clear()
+
+comsole.log("Cálculo do consumo de combustível")
+
+//entrada
+distancia = Number(input.question("Distancia percorrida"))
+litros = Number(input.question("Quantidade de litros de combustivel: "))
+
+//processamento 
+console.log(`Consumo do veículo: ${consumo.toFixed(1)} km/1`)
---
+++ exercicios/regra3.js
@@ -0,0 +1,22 @@
+/** Cálculo do consumo do combustível
+ * @author Wesley Souza 
+ */
+
+const input = require('readline-sync')
+
+// variaveis
+let x, y, valor
+
+console.clear()
+console.log("Regra de 3")
+console.log("x5 de y = valor")
+
+//entrada
+x = Number(input.question("Digite o valor de x: "))
+y = Number(input.question("Digite o valor de y: "))
+
+// Processamento 
+valor = (x * Y) / 100
+
+//saida
+console.log((`${x} `))
