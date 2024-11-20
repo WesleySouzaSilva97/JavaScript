1a3d62bcca8f8e14b1ddfa723f9b7101e0e7afcc
Wesley Souza <wesleyondo@gmail.com>
2024-11-19 22:22:04 -0300

    minecraft

--- poo/minecraft.js
+++ poo/minecraft.js
@@ -31,6 +31,39 @@ class Bloco {
     }
 }
 
+//herança
+// extends estabelece uma classe hierarquia
+
+class Enxada extends Bloco {
+    //atributos
+    constructor(textura, resistencia, conquista) {
+        super(textura, resistencia) //herança
+        this.conquista = conquista
+    }
+    //métodos
+    criarEnxada() {
+        console.log("-------------------------")
+        console.log("-_")
+        console.log(" /")
+        console.log(`Enxada de ${this.textura}`)
+        console.log(`Resistencia: ${this.resistencia}`)
+    }
+    // classe enxada herdou tudo da classe bloco
+    arar() {
+        console.log("._._._.")
+        if (this.conquista === true) {
+            console.log("☀ Conquista obtida!")
+        }
+
+    }
+
+    //Polimorfismo: Mudança de comportamento de um método que já existe na classe pai (modelo) no exemplo Bloco
+    // Atenção!! obrigatorio usar o mesmo nome do método
+    minerar() {
+        console.log("✞ Dano atribuído!")
+    }
+}
+
 /*** Mundo ***/
 console.log(" _____ _____ _____ _____ _____ _____ _____ _____ _____ ")
 console.log("|     |     |   | |   __|     | __  |  _  |   __|_   _|")
@@ -49,3 +82,15 @@ bloco2.construir()
 const bloco3 = new Bloco("Pedra", 5)
 bloco3.criarBloco()
 bloco3.minerar()
+
+const enxada1 = new Enxada("madeira", 2, false)
+enxada1.criarEnxada()
+enxada1.arar()
+
+const enxada2 = new Enxada("ferro", 5, true)
+enxada2.criarEnxada()
+enxada2.arar()
+
+const enxada3 = new Enxada("diamante", 10, false)
+enxada3.criarEnxada()
+enxada3.minerar()
